6c5836ed296515656d6a7d040479b68d2bef9827
Adel Kalu <adel.b.kalu@gmail.com>
2021-06-03 00:48:44 +0300

    all type of relytions

---
+++ School/School/Data/SchoolDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> students { get; set; }
+        public DbSet<Profile> profiles { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<StudentTeacher> studentTeachers { get; set; }
+
+
+    }
+}
