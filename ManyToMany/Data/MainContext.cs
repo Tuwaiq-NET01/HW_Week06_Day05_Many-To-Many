49516c512d3f054dce8bc821a01ea41af40d5d49
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-06-03 00:05:30 +0300

    finished project

---
+++ ManyToMany/Data/MainContext.cs
@@ -0,0 +1,27 @@
+﻿using ManyToMany.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManyToMany.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions<MainContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<StudentTeacher>()
+                .HasOne(st => st.Student)
+                .WithMany(students => students.StudentTeachers)
+                .HasForeignKey(st => st.StudentId);
+            
+            builder.Entity<StudentTeacher>()
+                .HasOne(st => st.Teacher)
+                .WithMany(teacher => teacher.TeacherStudents)
+                .HasForeignKey(st => st.TeacherId);
+        }
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<StudentTeacher> StudentsTeachers { get; set; }
+    }
+}
---
+++ ManyToMany/Models/StudentTeacher.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManyToMany.Models
+{
+    [Table("studentsteachers")]
+    public class StudentTeacher
+    {
+        public int ID { get; set; }
+        
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
